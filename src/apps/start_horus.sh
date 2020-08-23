04305c44f5505b108c15d6c16fb180bf9bf9179a
Mark Jessop <vk5qi@rfhead.net>
2020-08-23 16:39:22 +0930

    add strtup script

---
+++ src/apps/start_horus.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+#   LeanDVB DVB-S Decoder Script for Horus Launches
+#
+
+# RECEIVER SETTINGS
+# Receive Frequency, in Hz.
+RXFREQ=445000000
+
+# Receiver Gain Setting
+# Set to 0 to use AGC, otherwise use a gain value between 0 and 49
+GAIN=0
+
+# Sample Rate, in Hz
+# Suggest leaving this at 2.4 MHz.
+SAMPLERATE=2400000
+
+#
+# DVB TRANSMITTER SETTINGS
+#
+# Transmitter Symbol Rate
+SYMBRATE=1000e3
+# Transmitter FEC
+FEC=1/2
+
+
+# Save captured Transport Stream to this file.
+# Note that this will be overwritten each time this script is run.
+TSOUT=dvb_capture.ts
+
+
+
+# Configure the gain setting
+GAIN_SETTING=""
+if [ "$GAIN" = "0" ]; then
+	echo "Using AGC."
+	GAIN_SETTING=""
+else
+	echo "Using Manual Gain"
+	GAIN_SETTING=" -g $GAIN"
+fi
+
+# Start the decoder
+
+rtl_sdr -f $RXFREQ $GAIN_SETTING -s $SAMPLERATE - | ./leandvb --gui --inpipe $SAMPLERATE --sr $SYMBRATE --cr $FEC | tee $TSOUT | mplayer -cache 32 -
